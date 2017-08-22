787c79517ca8b563f7ff878a628dc9fa28068834
shilpa155 <reddy.shilpa155@gmail.com>
2017-08-22 10:32:46 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.get('/article-two', function (req, res) {
 app.get('/article-three', function (req, res) {
   res.sendFile('Article three is required and will be strted here');
 });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
