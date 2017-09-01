b342e0e26cb3abe56cc2adccebe53560848589e0
shilpa155 <reddy.shilpa155@gmail.com>
2017-09-01 14:14:49 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
 var element = document.getElementById('madi');
-img.onclick = function() {
+    img.onclick = function() {
     img.style.marginLeft = '100px';
 }
