5b721a7f76a28a110df6d77ffa2e2e37bca01ba9
shilpa155 <reddy.shilpa155@gmail.com>
2017-09-01 14:10:04 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+var element = document.getElementById('img');
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+}
