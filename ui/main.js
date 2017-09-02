b1a07873cee566695fc99de590e31240eade0877
shilpa155 <reddy.shilpa155@gmail.com>
2017-09-02 01:24:54 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 
-/*var element = document.getElementById('main-text');
+var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
 var img = document.getElementById('madi');
@@ -13,3 +13,17 @@ var img = document.getElementById('madi');
     img.onclick = function() {
     var interval = setInterval(moveRight, 100);
 };*/
+
+// counter code
+var button = document.getElementById('counter');
+button.onClick = function () {
+    //Make request to the endpoint
+    
+    //Capture the response store it in a variable
+    
+    // render the variable in the correct span
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
+    
+}
