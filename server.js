03bc0fc5cd66f67eeab3a36b81554e99b3a49f7b
shilpa155 <reddy.shilpa155@gmail.com>
2017-09-02 06:55:57 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,10 +80,10 @@ app.get('/', function (req, res) {
 });
 
 var counter = 0;
-app.get('/counter', function (req, res) {
-    counter = counter + 1;
-    res.send(counter.toString());
-});
+//app.get('/counter', function (req, res) {
+//    counter = counter + 1;
+//    res.send(counter.toString());
+//});
 app.get('/:articleName', function (req, res) {
     articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));
