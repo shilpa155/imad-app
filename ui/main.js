ba4674d58dc4ca5550d33c65b04b50cab653228c
shilpa155 <reddy.shilpa155@gmail.com>
2017-09-02 06:27:55 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,12 +19,26 @@ var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function () {
     //Make request to the endpoint
+    var request = new XMLHttpRequest();
     
     //Capture the response store it in a variable
+    request.onreadystatechange = function () {
+       if(request.readystate === XMLHttpRequest.DONE) {
+           // take some action
+           if(request.status === 200) {
+           var counter = request.resonceText;
+           }
+       } 
+       // not done yet
+       
+    };
     
-    // render the variable in the correct span
+    // make a request
+    request.open = ('GET' , 'http://reddyshilpa155.imad.hasura-app.io/counter' , true);
+    request.send(null);
+    /* render the variable in the correct span
     counter = counter + 1;
     var span = document.getElementById("count");
     span.innerHTML = counter.toString();
-    
-}
+    */
+};
