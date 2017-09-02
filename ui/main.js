24fb013eef887a3d701241224bce37197e51dae2
shilpa155 <reddy.shilpa155@gmail.com>
2017-09-02 12:08:47 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -50,15 +50,34 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
 // make request tot theserver send the name
-
-// capture the list of name and render it as list
-var names = ['Name1' , 'Name2' , 'Name3' ,'Name4'];
+ var request = new XMLHttpRequest();
+    
+    //Capture the response store it in a variable
+    request.onreadystatechange = function () {
+       if(request.readyState === XMLHttpRequest.DONE) {
+           // take some action
+           if(request.status === 200) {
+               var names = request.responseText;
+               names = JSON.parse(names);
+           //var names = ['Name1' , 'Name2' , 'Name3' ,'Name4'];
 var list = '';
 for (var i=0; i< names.length; i++) {
     list += '<li>' +names[i] + '</li>';
-    
 }
 var ul = document.getElementById('namelist');
 ul.innerHTML = list;
+    
+           }
+       } 
+       // not done yet
+       
+    };
+    
+    // make a request
+    request.open('GET', 'http://reddyshilpa155.imad.hasura-app.io/submit-name?name=', + name, true);
+    request.send(null);
+// capture the list of name and render it as list
+
+
 
 };
