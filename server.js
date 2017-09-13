8b69923cc7ffcc7b21cd585fff77295acf895aef
shilpa155 <reddy.shilpa155@gmail.com>
2017-09-13 07:35:59 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var pool = new pool(config);
+var pool = new Pool(config);
 app.get('/test-db', function(req, res){
     //make a select request
     //return responce with the result
