0ef9ab743b835dc96b8b6a1601e7e13fe91b43e0
shilpa155 <reddy.shilpa155@gmail.com>
2017-09-14 01:08:54 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.get('/articles/:articleName', function (req, res) {
     //var articleName = req.params.articleName;
     //SELECE * FROM article WHERE title = article - one
 //SELECE * FROM article WHERE title = '\';DELETE where a=\'afdf'
-    pool.query("SELECT * FROM article WHERE title = '"  + req.params.articleName + "'", function(err,result){
+    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err,result){
         if(err){
             res.status(500).send(err.toString());
         } else {
