fb031ebc0f745fce1b3f3f0d5564843d1ac3580d
shilpa155 <reddy.shilpa155@gmail.com>
2017-09-15 11:51:51 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/', function (req, res) {
 
 function hash (input, salt){
     //how we create hash?
-    var hashed = crypto.pdkdf2Sync(input, salt, 10000, 512, 'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
 }
 
